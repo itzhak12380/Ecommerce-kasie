4b90fb32b54481850a2933f778b328b69c1dd301
itzhak12380 <iascc12380@gmail.com>
2022-01-30 23:35:41 +0200

    virsion 1.0.0

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -27,6 +27,7 @@ class APIfeatures {
         const page = this.queryString.page * 1 || 1
         const limit = this.queryString.limit * 1 || 9
         const skip = (page - 1) * limit
+        console.log(this.query.skip);
         this.query = this.query.skip(skip).limit(limit)
         return this;
     }
